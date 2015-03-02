851e859a5f57b803c106e3b0d4d950c9e0dd4681
deryrahma <halohalobandung88@gmail.com>
2015-03-02 07:59:43 +0700

    update banyak

---
+++ Dolly_MPPL/index2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Basic Bootstrap Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <script src="jquery/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <h1>Hello, world!</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+            <div class="col-md-6"><p>Box 1</p></div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
