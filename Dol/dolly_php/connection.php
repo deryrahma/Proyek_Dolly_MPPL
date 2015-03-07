2ae31117f3135b31c585892663e2e857552c3d64
Reva Yoga Pradana <ryp_reaper@yahoo.com>
2015-03-08 01:53:25 +0700

    fiksasi form di front end tambah data siswa. fiksasi back end tambah data siswa. perbaikan connection.php

--- Dol/dolly_php/connection.php
+++ Dol/dolly_php/connection.php
@@ -1,13 +1,15 @@
 <?php
-	$username = "umum";
+	$username = "mpplbaru";
 	$password = "";
 	$hostname = "10.151.31.107"; 
 
 	//connection to the database
-	$conn = mysql_connect($hostname, $username, $password, "mppl_baru") 
+	$conn = mysql_connect($hostname, $username, $password) 
 	 or die("Unable to connect to MySQL");
 	//echo "Connected to MySQL<br>";
 
+	mysql_select_db("mppl_baru") or die("could not select database");
+	//echo "fine";
 	//close the connection
 	//mysql_close($conn);
 ?>
